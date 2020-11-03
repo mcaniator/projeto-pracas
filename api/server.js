67e749c02b73a6e6fd437365fae2e231484c0b99
= <vinicius.carlos@live.com>
2020-11-03 11:07:59 -0300

    Use nodemon to listen server

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send('Hello Vinimaan');
+    res.send('Hello Vinimaan!');
 })
 
 app.listen(3001);
