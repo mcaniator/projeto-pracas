7f72409ae9e3af9f1aa1336b0facee7f0c9268c0
= <vinicius.carlos@live.com>
2020-11-17 11:23:19 -0300

    Uso de desestruturaçao na importaçao de icones do material-ui

--- front/src/views/admin/Dashboard/Dashboard.js
+++ front/src/views/admin/Dashboard/Dashboard.js
@@ -1,21 +1,25 @@
 import React from "react";
+
 // react plugin for creating charts
 import ChartistGraph from "react-chartist";
+
 // @material-ui/core
 import { makeStyles } from "@material-ui/core/styles";
 import Icon from "@material-ui/core/Icon";
+
 // @material-ui/icons
-import Store from "@material-ui/icons/Store";
-import Warning from "@material-ui/icons/Warning";
-import DateRange from "@material-ui/icons/DateRange";
-import LocalOffer from "@material-ui/icons/LocalOffer";
-import Update from "@material-ui/icons/Update";
-import ArrowUpward from "@material-ui/icons/ArrowUpward";
-import AccessTime from "@material-ui/icons/AccessTime";
-import Accessibility from "@material-ui/icons/Accessibility";
-import BugReport from "@material-ui/icons/BugReport";
-import Code from "@material-ui/icons/Code";
-import Cloud from "@material-ui/icons/Cloud";
+import { Warning, Store, DateRange ,LocalOffer, Update, ArrowUpward, AccessTime, Accessibility, BugReport, Code, Cloud}  from "@material-ui/icons";
+//import Warning from "@material-ui/icons/Warning";
+//import DateRange from "@material-ui/icons/DateRange";
+//import LocalOffer from "@material-ui/icons/LocalOffer";
+//import Update from "@material-ui/icons/Update";
+//import ArrowUpward from "@material-ui/icons/ArrowUpward";
+// import AccessTime from "@material-ui/icons/AccessTime";
+// import Accessibility from "@material-ui/icons/Accessibility";
+// import BugReport from "@material-ui/icons/BugReport";
+// import Code from "@material-ui/icons/Code";
+// import Cloud from "@material-ui/icons/Cloud";
+
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
