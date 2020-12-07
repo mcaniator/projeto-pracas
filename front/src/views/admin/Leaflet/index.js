cefbbb5751fdf4a2c9408bf9e7b58b273846cbf8
Edson <edsonjunior@ice.ufjf.br>
2020-12-07 10:03:26 -0300

    adicionar funcionalidade de desenho de poligonos

--- front/src/views/admin/Leaflet/index.js
+++ front/src/views/admin/Leaflet/index.js
@@ -9,6 +9,7 @@ import {
   Popup,
   Rectangle,
   TileLayer,
+  Polygon
 } from 'react-leaflet'
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
@@ -17,9 +18,6 @@ import CardHeader from "components/Card/CardHeader.js";
 import CardBody from "components/Card/CardBody.js";
 
 import SimpleModal from "components/SimpleModal/SimpleModal.js"
-
-
-import './style.css'
 
 const { BaseLayer, Overlay } = LayersControl
 
@@ -40,65 +38,41 @@ export default class Leaflet extends Component<{}, State> {
     lat: -21.7642,
     lng: -43.3496,
     zoom: 13,
+    currentPos: 0,
+    positions: []
   }
+  //state.positions.concat([newPos])
+
+  handleClick(e){
+    const newPos = [e.latlng.lat, e.latlng.lng];
+  this.setState((state) => {
+  return {positions: state.positions.concat([newPos])}
+  });
+}
 
   render() {
     const position = [this.state.lat, this.state.lng]
     return (
       <GridContainer>
         <GridItem xs={12} sm={12} md={12}>
-          <Map center={position} zoom={this.state.zoom} style={{ height: '100vh', width: '100%' }}>
+          <Map center={position} zoom={this.state.zoom} style={{ height: '100vh', width: '100%' }} onClick={this.handleClick.bind(this)}>
 
-          <LayersControl position="topright">
-              <BaseLayer checked name="OpenStreetMap.Mapnik">
                 <TileLayer
                   attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                   url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-              </BaseLayer>
-              <BaseLayer name="OpenStreetMap.BlackAndWhite">
-                <TileLayer
-                  attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png"
-                />
-              </BaseLayer>
-              <Overlay /*checked */ name="Layer group with circles">
-                <LayerGroup>
-                  <Circle center={center} fillColor="blue" radius={200} />
-                  <Circle
-                    center={center}
-                    fillColor="red"
-                    radius={100}
-                    stroke={false}
-                  />
-                  <LayerGroup>
-                    <Circle
-                      center={[51.51, -0.08]}
-                      color="green"
-                      fillColor="green"
-                      radius={100}
-                    />
-                  </LayerGroup>
-                </LayerGroup>
-              </Overlay>
-              <Overlay name="Feature group">
-                <FeatureGroup color="purple">
-                  <Popup>Popup in FeatureGroup</Popup>
-                  <Circle center={center} radius={200} />
-                  <Rectangle bounds={rectangle} />
-                </FeatureGroup>
-              </Overlay>
-            </LayersControl>
-            <TileLayer
-              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
                 <Marker position={center}>
                   <Popup>
                     A pretty CSS3 popup. <br /> Easily customizable.
                     <SimpleModal/>
                   </Popup>
                 </Marker>
+
+                <TileLayer
+                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                />
+
+                <Polygon positions={this.state.positions} color="blue" />
           </Map>
 
         </GridItem>
