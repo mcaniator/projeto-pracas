8fe374ccc84ff2980590c30138eed54bd1172247
Edson <edsonjunior@ice.ufjf.br>
2020-12-07 17:45:41 -0300

    adicionar opcoes a respeito do desenho dos poligonos

--- front/src/views/admin/Leaflet/index.js
+++ front/src/views/admin/Leaflet/index.js
@@ -1,13 +1,9 @@
 import React, { Component } from 'react'
 import {
-  Circle,
-  FeatureGroup,
-  LayerGroup,
   LayersControl,
   Map,
   Marker,
   Popup,
-  Rectangle,
   TileLayer,
   Polygon
 } from 'react-leaflet'
@@ -15,6 +11,8 @@ import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
 
 import SimpleModal from "components/SimpleModal/SimpleModal.js"
+
+import Control from 'react-leaflet-control';
 
 const { BaseLayer, Overlay } = LayersControl
 
@@ -31,45 +29,112 @@ const rectangle = [
 ]
 
 export default class Leaflet extends Component<{}, State> {
+
   state = {
     lat: -21.7642,
     lng: -43.3496,
     zoom: 13,
-    currentPos: 0,
-    positions: []
+    currentPolygon: [],
+    polygonList: [{id : 0, 
+                   coordinates : [[-21.784528085620426, -43.35050582885743],
+                                  [-21.751368561385405, -43.33505630493164],
+                                  [-21.740845188197625, -43.38140487670899],
+                                  [-21.773210071085067, -43.38277816772462]]
+                  }],
+    drawActive: false
   }
-  //state.positions.concat([newPos])
 
-  handleClick(e){
-    const newPos = [e.latlng.lat, e.latlng.lng];
-  this.setState((state) => {
-  return {positions: state.positions.concat([newPos])}
-  });
-}
+  handleClick(e)
+  {
+    if (this.state.drawActive)
+    {
+      const newPos = [e.latlng.lat, e.latlng.lng];
+      this.setState((state) => {
+        return {currentPolygon: state.currentPolygon.concat([newPos])}
+      });
+    }
+  }
+
+  startDraw()
+  {
+    this.setState({drawActive: true})
+  }
+
+  addPolygon()
+  {
+    this.setState({drawActive: false})
+
+
+    if(this.state.currentPolygon.length > 2)
+    {
+      this.setState((state) => {
+        return {
+          drawActive: false,
+          polygonList: state.polygonList.concat({id: state.polygonList.length,  coordinates: state.currentPolygon})
+        }
+      });
+    }
+    this.clearDraw();
+  }
+
+  clearDraw()
+  {
+    this.setState({currentPolygon: []});
+  }
 
   render() {
-    const position = [this.state.lat, this.state.lng]
+    const position = [this.state.lat, this.state.lng];
+    
     return (
       <GridContainer>
         <GridItem xs={12} sm={12} md={12}>
           <Map center={position} zoom={this.state.zoom} style={{ height: '100vh', width: '100%' }} onClick={this.handleClick.bind(this)}>
 
-                <TileLayer
-                  attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={center}>
-                  <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                    <SimpleModal/>
-                  </Popup>
-                </Marker>
+            <Control position="topleft" >
+              <button 
+                onClick={ this.startDraw.bind(this) }
+                style={{color: "#f00"}}
+              >
+                Começar Desenho
+              </button>
+            </Control>
 
-                <TileLayer
-                    url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-                />
+            <Control position="topleft" >
+              <button 
+                onClick={ this.addPolygon.bind(this) }
+              >
+                Adicionar Poligono
+              </button>
+            </Control>
 
-                <Polygon positions={this.state.positions} color="blue" />
+            <Control position="topleft" >
+              <button 
+                onClick={ this.clearDraw.bind(this) }
+              >
+                Descartar Desenho
+              </button>
+            </Control>
+
+            <Marker position={center}>
+              <Popup>
+                A pretty CSS3 popup. <br /> Easily customizable.
+                <SimpleModal/>
+              </Popup>
+            </Marker>
+
+            <TileLayer
+                url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            />
+
+            <Polygon key="drawPolygon" positions={this.state.currentPolygon} color="blue" />
+            {this.state.polygonList.map(polygon => (
+              <Polygon key={polygon.id} positions={polygon.coordinates} color="red" />
+            ))}
+
+            <TileLayer
+              attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
           </Map>
 
         </GridItem>
