5bc857ccd6a000179d7cbb3466ac303a06238a62
FilipeBrinati <44785517+FilipeBrinati@users.noreply.github.com>
2022-05-17 12:59:10 -0300

    Update FormTest.jsx
    adição das perguntas e categorias no form sem conflito

--- front/src/views/admin/CadastroForm/components/FormTest.jsx
+++ front/src/views/admin/CadastroForm/components/FormTest.jsx
@@ -4,8 +4,9 @@ import form from "../form.json";
 import { useState } from "react";
 import { Divider } from '@material-ui/core';
 import Button from "@material-ui/core/Button";
-import category from "../category.json"
-import form_field from "../form_field.json"
+import Category from "../category.json";
+import form_field from "../form_field.json";
+import form_test from "../form_test.json";
 
 import axios from "axios";
 
@@ -17,6 +18,65 @@ const FieldInCategory = (category, field) =>
         return false;
 };
 
+const AddQuestionToForm = (id) =>
+{
+    var check = 0;
+    if(form_test[0].question.includes(id)) {
+        check = 1;
+    }
+    if(check == 0){
+        form_test[0].question.push(id);    
+    }    
+};
+
+const AddCategoryToForm = (id) =>
+{
+    var check = 0;
+    if(form_test[0].category.includes(id)) {
+        check = 1;
+    }
+    if(check == 0){
+        form_test[0].category.push(id);
+        for(var i in form_field){
+            if(FieldInCategory(id, form_field[i].category_id)){
+                AddQuestionToForm(form_field[i].id);
+            }
+        }  
+    }
+};
+
+const AuxLoadCategory = (id) => {
+    if(form_test[0].category.includes(id)){
+        for(var i in Category){
+        if(Category[i].id == id)
+            return Category[i].name;
+        }
+    }    
+};
+
+const AuxLoadQuestion = (id) => {
+    if(form_test[0].question.includes(id)){
+        for(var i in form_field){
+        if(form_field[i].id == id)
+            return form_field[i].name;
+        }
+    }    
+};
+
+const LoadCategory = () => {
+    const array = []
+    var check = 0;
+
+    for(var i in form_test[0].category){
+        array.push(AuxLoadCategory(form_test[0].category[i]));
+        for(var k in form_test[0].question){
+            array.push(AuxLoadQuestion(form_test[0].question[k]))
+        }
+    }
+    console.log(array);
+    return array;
+};
+
 export function FormTest(){
     return (
         <div>
@@ -24,17 +84,17 @@ export function FormTest(){
                 Gerador de Formulários
             </div>
             &nbsp;
-            {category.map(e=>(
+            {Category.map(e=>(
                 <div>
                     <div style={{fontSize:"26px"}}>
-                        <Button style={{marginRight: "25px"}} > ADD </Button>
+                        <Button onClick={() => AddCategoryToForm(e.id)} style={{marginRight: "25px"}} > ADD </Button>
                         {e.name}
                     </div>
                     <Divider variant="middle" />
                     {form_field.map(f =>(
                         FieldInCategory(e.id, f.category_id) ?(
                             <div>
-                                <Button style={{marginRight: "25px", marginLeft: "25px"}} > ADD </Button>
+                                <Button onClick={() => AddQuestionToForm(f.id)} style={{marginRight: "25px", marginLeft: "25px"}} > ADD </Button>
                                 {f.name}
                             </div>
                         ) : (<></>)
@@ -44,7 +104,10 @@ export function FormTest(){
             ))}
 
             <Divider variant="middle" />
-
+            
+            <div>
+                {LoadCategory()}
+            </div>
 
         </div>
     );
