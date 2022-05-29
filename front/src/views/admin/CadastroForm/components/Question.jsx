cc5068ab8fa94cf9072dc95988b7418967d5e6ac
Paulo Rozatto <rozatto.paulo.victot@gmail.com>
2022-05-29 19:54:41 -0300

    campo de opcao de limite de caractere para pergunta de texto

--- front/src/views/admin/CadastroForm/components/Question.jsx
+++ front/src/views/admin/CadastroForm/components/Question.jsx
@@ -95,7 +95,9 @@ export default class Question extends React.Component {
                     }
                     break;
                 case 'text':
-                    field = {}
+                    let char_limit = parseInt(document.getElementById('limit').value.trim()) || null;
+
+                    field = { char_limit }
                     break;
                 case 'option':
                     field = {
@@ -175,139 +177,164 @@ export default class Question extends React.Component {
 
     render() {
         return (
-            <GridContainer style={{ paddingLeft: '2em', paddingRight: '4em' }}>
-                <GridItem xs={12} md={6}>
-                    <CustomInput
-                        labelText="Pergunta"
-                        id="question"
-                        formControlProps={{
-                            fullWidth: true,
-                        }}
+            <>
+                <GridContainer style={{ paddingLeft: '2em', paddingRight: '4em' }}>
+                    <GridItem xs={12} md={6}>
+                        <CustomInput
+                            labelText="Pergunta"
+                            id="question"
+                            formControlProps={{
+                                fullWidth: true,
+                            }}
 
-                        inputProps={{
-                            type: "text",
-                        }}
-                    ></CustomInput>
-                </GridItem>
-                <GridItem xs={12} md={2}>
-                    <Select
-                        id="select-type"
-                        name="type"
-                        value={this.state.type ? this.state.type : 0}
-                        style={selectStyle}
-                        renderValue={(selected) => {
-                            switch (selected) {
-                                case 0: {
-                                    return <span style={{ color: '#AAAAB2' }}>Tipo</span>;
+                            inputProps={{
+                                type: "text",
+                            }}
+                        ></CustomInput>
+                    </GridItem>
+                    <GridItem xs={12} md={2}>
+                        <Select
+                            id="select-type"
+                            name="type"
+                            value={this.state.type ? this.state.type : 0}
+                            style={selectStyle}
+                            renderValue={(selected) => {
+                                switch (selected) {
+                                    case 0: {
+                                        return <span style={{ color: '#AAAAB2' }}>Tipo</span>;
+                                    }
+                                    default:
+                                        return selected;
                                 }
-                                default:
-                                    return selected;
-                            }
-                        }}
-                        onChange={this.handleChange}
-                    >
-                        <MenuItem value={0} disabled>Tipo</MenuItem>
-                        <MenuItem value={'Texto'}>Texto</MenuItem>
-                        <MenuItem value={'Numérico'}>Numérico</MenuItem>
-                        <MenuItem value={'Opções'}>Opções</MenuItem>
+                            }}
+                            onChange={this.handleChange}
+                        >
+                            <MenuItem value={0} disabled>Tipo</MenuItem>
+                            <MenuItem value={'Texto'}>Texto</MenuItem>
+                            <MenuItem value={'Numérico'}>Numérico</MenuItem>
+                            <MenuItem value={'Opções'}>Opções</MenuItem>
 
-                    </Select>
-                </GridItem>
-                <GridItem xs={12} md={4}>
-                    <Select
-                        id="select-category"
-                        name="category"
-                        value={this.state.category ? this.state.category : 0}
-                        style={selectStyle}
-                        renderValue={(selected) => {
-                            switch (selected) {
-                                case 0: {
-                                    return <span style={{ color: '#AAAAB2' }}>Categoria</span>;
+                        </Select>
+                    </GridItem>
+                    <GridItem xs={12} md={4}>
+                        <Select
+                            id="select-category"
+                            name="category"
+                            value={this.state.category ? this.state.category : 0}
+                            style={selectStyle}
+                            renderValue={(selected) => {
+                                switch (selected) {
+                                    case 0: {
+                                        return <span style={{ color: '#AAAAB2' }}>Categoria</span>;
+                                    }
+
+                                    default:
+                                        return this.props.categories.find(e => e.id == selected).name;
                                 }
+                            }}
+                            onChange={this.handleChange}
+                        >
+                            <MenuItem value={0} disabled>Categoria</MenuItem>
+                            <MenuItem value={0}>
+                                <RegularButton color="info" onClick={this.showDialog} style={{ width: '100%' }}>
+                                    <Add />
+                                    Nova Categoria
+                                </RegularButton>
+                            </MenuItem>
+                            {this.props.categories.map(cat => <MenuItem key={cat.id} value={cat.id}>{cat.name}</MenuItem>)}
+                        </Select>
+                    </GridItem>
 
-                                default:
-                                    return this.props.categories.find(e => e.id == selected).name;
-                            }
-                        }}
-                        onChange={this.handleChange}
-                    >
-                        <MenuItem value={0} disabled>Categoria</MenuItem>
-                        <MenuItem value={0}>
-                            <RegularButton color="info" onClick={this.showDialog} style={{ width: '100%' }}>
-                                <Add />
-                                Nova Categoria
-                            </RegularButton>
-                        </MenuItem>
-                        {this.props.categories.map(cat => <MenuItem key={cat.id} value={cat.id}>{cat.name}</MenuItem>)}
-                    </Select>
-                </GridItem>
+                    {this.state.type == 'Texto' &&
+                        <>
+                            <GridItem xs={12} md={4}>
+                                <CustomInput
+                                    labelText="Limite de character"
+                                    id="limit"
+                                    formControlProps={{
+                                        fullWidth: true,
+                                    }}
 
-
-                {this.state.type == 'Opções' &&
-                    <>
-                        <GridItem xs={12} md={8}>
-                            <CustomInput
-                                labelText="Nova opcao"
-                                id="option"
-                                formControlProps={{
-                                    fullWidth: true,
-                                }}
-
-                                inputProps={{
-                                    type: "text",
-                                }}
-                            ></CustomInput>
-                        </GridItem>
-
-                        <GridItem xs={12} md={4}>
-                            <RegularButton style={selectStyle} color="info" onClick={this.addOption}>Adcionar</RegularButton>
-                        </GridItem>
-
-                        <GridItem xs={12} md={12}>
-                            <Paper>
-                                <List sx={{
-                                    width: '100%',
-                                    maxWidth: 360,
-                                    bgcolor: 'background.paper',
-                                    position: 'relative',
-                                    overflow: 'auto',
-                                    maxHeight: 50,
-                                    '& ul': { padding: 0 },
-                                }}>
-                                    {this.state.options.length == 0 &&
-                                        <ListItem>
-                                            <ListItemText key={0} primary={"Adcione opções"} />
-                                        </ListItem>}
-                                    {this.state.options}
-                                </List>
-                            </Paper>
-                        </GridItem>
-                    </>
-                }
+                                    inputProps={{
+                                        type: "number",
+                                        inputProps: {
+                                            min: 1,
+                                        },
+                                    }}
+                                ></CustomInput>
+                            </GridItem>
+                            <br />
+                        </>
+                    }
 
 
-                <GridItem>
-                    <RegularButton color="primary" onClick={this.inserQuestion}>Adcionar</RegularButton>
-                    <Snackbar
-                        place={"tc"}
-                        color="success"
-                        message="Nova pergunta inserida"
-                        open={this.state.showSuccess}
-                        closeNotification={() => { this.closeMessage('success') }}
-                        close
-                    ></Snackbar>
+                    {this.state.type == 'Opções' &&
+                        <>
+                            <GridItem xs={12} md={8}>
+                                <CustomInput
+                                    labelText="Nova opcao"
+                                    id="option"
+                                    formControlProps={{
+                                        fullWidth: true,
+                                    }}
 
-                    <Snackbar
-                        place={"tc"}
-                        color="warning"
-                        message="Preencha todos os campos!"
-                        open={this.state.showError}
-                        closeNotification={() => { this.closeMessage('error') }}
-                        close
-                    ></Snackbar>
-                </GridItem>
-                <DialogCategory open={this.state.showDialog} close={this.insertCategory}></DialogCategory>
-            </GridContainer>
+                                    inputProps={{
+                                        type: "text",
+                                    }}
+                                ></CustomInput>
+                            </GridItem>
+
+                            <GridItem xs={12} md={4}>
+                                <RegularButton style={selectStyle} color="info" onClick={this.addOption}>Adcionar</RegularButton>
+                            </GridItem>
+
+                            <GridItem xs={12} md={12}>
+                                <Paper>
+                                    <List sx={{
+                                        width: '100%',
+                                        maxWidth: 360,
+                                        bgcolor: 'background.paper',
+                                        position: 'relative',
+                                        overflow: 'auto',
+                                        maxHeight: 50,
+                                        '& ul': { padding: 0 },
+                                    }}>
+                                        {this.state.options.length == 0 &&
+                                            <ListItem>
+                                                <ListItemText key={0} primary={"Adcione opções"} />
+                                            </ListItem>}
+                                        {this.state.options}
+                                    </List>
+                                </Paper>
+                            </GridItem>
+                        </>
+                    }
+
+                </GridContainer>
+                <GridContainer style={{ paddingLeft: '2em', paddingRight: '4em' }}>
+                    <GridItem>
+                        <RegularButton color="primary" onClick={this.inserQuestion}>Adcionar</RegularButton>
+                        <Snackbar
+                            place={"tc"}
+                            color="success"
+                            message="Nova pergunta inserida"
+                            open={this.state.showSuccess}
+                            closeNotification={() => { this.closeMessage('success') }}
+                            close
+                        ></Snackbar>
+
+                        <Snackbar
+                            place={"tc"}
+                            color="warning"
+                            message="Preencha todos os campos!"
+                            open={this.state.showError}
+                            closeNotification={() => { this.closeMessage('error') }}
+                            close
+                        ></Snackbar>
+                    </GridItem>
+                    <DialogCategory open={this.state.showDialog} close={this.insertCategory}></DialogCategory>
+                </GridContainer>
+            </>
         );
     }
 }
