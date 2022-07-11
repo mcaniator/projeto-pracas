0fbf94dba08711c79939e5774904b209b8606d06
Samuel Abreu <samuelapp@outlook.com>
2022-07-11 17:02:33 -0300

    feat: adiciona visualizacao de formulario na parte de criacao de forms
    O codigo ta feio, ta, fica a vontade pra mexer

---
+++ front/src/routesAdmin_LOCAL_11728.js
@@ -0,0 +1,149 @@
+/*!
+
+=========================================================
+* Material Dashboard React - v1.9.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/material-dashboard-react
+* Copyright 2020 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/material-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+// @material-ui/icons
+import Dashboard from "@material-ui/icons/Dashboard";
+import Person from "@material-ui/icons/Person";
+import LibraryBooks from "@material-ui/icons/LibraryBooks";
+import BubbleChart from "@material-ui/icons/BubbleChart";
+import LocationOn from "@material-ui/icons/LocationOn";
+import Notifications from "@material-ui/icons/Notifications";
+import AddAlert from "@material-ui/icons/AddAlert";
+import AssignmentIcon from '@material-ui/icons/Assignment';
+
+// core components/views for Admin layout
+import DashboardPage from "views/admin/Dashboard";
+import TableList from "views/admin/TableList";
+import Typography from "views/admin/Typography";
+import Icons from "views/admin/Icons";
+import NotificationsPage from "views/admin/Notifications";
+import Leaflet from "views/admin/Leaflet";
+import SweetAlert from "views/admin/SweetAlert"
+
+
+// project praças
+import Pracas from "views/admin/Pracas";
+import Users from "views/admin/Users";
+
+
+import Home from "views/website/Home";
+
+
+import CreateUser from "views/admin/Users/UserCreate";
+import EditUser from "views/admin/Users/UserEdit";
+import ShowUser from "views/admin/Users/UserShow";
+import Forms from "views/admin/Forms/index.js";
+
+
+const dashboardRoutes = [
+  {
+    path: "/pracas",
+    name: "Praças",
+    icon: Dashboard,
+    component: Pracas,
+    layout: "/admin"
+  },
+  {
+    path: "/usuarios",
+    name: "Usuários",
+    icon: Person,
+    component: Users,
+    layout: "/admin"
+  },
+  {
+    path: "/usuarios-criar",
+    name: "Criar usuário",
+    icon: Person,
+    component: CreateUser,
+    layout: "/admin",
+    notRenderOnSidebar: true,
+  },
+  {
+    path: "/usuarios-editar",
+    name: "Editar Usuário",
+    icon: Person,
+    component: EditUser,
+    layout: "/admin",
+    notRenderOnSidebar: true,
+  },
+  {
+    path: "/usuarios-mostrar",
+    name: "Mostrar Usuário",
+    icon: Person,
+    component: ShowUser,
+    layout: "/admin",
+    notRenderOnSidebar: true,
+  },
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: Dashboard,
+    component: DashboardPage,
+    layout: "/admin"
+  },
+  {
+    path: "/table",
+    name: "Table List",
+    icon: "content_paste",
+    component: TableList,
+    layout: "/admin"
+  },
+  {
+    path: "/typography",
+    name: "Typography",
+    icon: LibraryBooks,
+    component: Typography,
+    layout: "/admin"
+  },
+  {
+    path: "/icons",
+    name: "Icons",
+    icon: BubbleChart,
+    component: Icons,
+    layout: "/admin"
+  },
+  {
+    path: "/notifications",
+    name: "Notifications",
+    icon: Notifications,
+    component: NotificationsPage,
+    layout: "/admin"
+  },
+  {
+    path: "/leaflet",
+    name: "Leaflet",
+    icon: LocationOn,
+    component: Leaflet,
+    layout: "/admin"
+  },
+  {
+    path: "/sweet-alert",
+    name: "Sweet Alert",
+    icon: AddAlert,
+    component: SweetAlert,
+    layout: "/admin"
+  },
+  {
+    path: "/forms",
+    name: "Formulario",
+    icon: AssignmentIcon,
+    component: Forms,
+    layout: "/admin"
+  },
+];
+
+export default dashboardRoutes;
--- front/src/views/admin/CadastroForm/components/FormMount.jsx
+++ front/src/views/admin/CadastroForm/components/FormMount.jsx
@@ -2,6 +2,8 @@ import { Divider, InputLabel, MenuItem, Select } from "@material-ui/core";
 import GridContainer from "components/Grid/GridContainer";
 import GridItem from "components/Grid/GridItem";
 import React from "react";
+import Form from "@rjsf/material-ui";
+import { Category } from "@material-ui/icons";
 
 const selectStyle = {
     display: 'block',
@@ -11,6 +13,55 @@ const selectStyle = {
 
 const labelStyle = {
     marginTop: '1.69em',
+}
+
+class Opcao{
+    constructor(nome){
+        // this.limiteOpcoes = limiteOpcoes;
+        // this.totalOpcoes = totalOpcoes;
+        // this.visualPreferencia = visualPreferencia;
+        this.nome = nome;
+    }
+}
+class CampoOpcoes{
+    constructor(limiteOpcoes, totalOpcoes, visualPreferencia, opcoes){
+        this.limiteOpcoes = limiteOpcoes;
+        this.totalOpcoes = totalOpcoes;
+        this.visualPreferencia = visualPreferencia;
+        this.opcoes = opcoes;
+    }
+}
+class CampoNumerico{
+    constructor(min, max){
+        this.min = min;
+        this.max = max;
+    }
+}
+class CampoTexto{
+    constructor(limiteCaracteres){
+        this.limiteCaracteres = limiteCaracteres;
+    }
+}
+class Categoria{
+    constructor(opcional, ativo, nome, campos){
+        this.opcional = opcional;
+        this.ativo = ativo;
+        this.nome = nome;
+        this.campos = campos;
+    }
+}
+class CampoFormulario{
+    constructor(nome, opcional, ativo, campo){
+        this.nome = nome;
+        this.opcional = opcional;
+        this.ativo = ativo;
+        this.campo = campo
+    }
+}
+class EstruturaFormulario{
+    constructor(categorias){
+        this.categorias = categorias;
+    }
 }
 
 export default class FormMount extends React.Component {
@@ -38,6 +89,7 @@ export default class FormMount extends React.Component {
                 this.setState({
                     formsHeaders: headers.data,
                 })
+                console.log(this.state.formsHeaders)
             }
         } catch (e) {
             console.error(e);
@@ -54,6 +106,9 @@ export default class FormMount extends React.Component {
 
         if (form) {
             this.setState({ selectedForm: form });
+            console.log(form)
+                console.log("FORMSSSSS")
+                console.log(this.state.forms)
         }
         else {
             try {
@@ -63,6 +118,8 @@ export default class FormMount extends React.Component {
                     forms: [...this.state.forms, res.data],
                     selectedForm: res.data
                 })
+                console.log("FORMSSSSS")
+                console.log(this.state.forms)
 
                 console.log(res.data);
             } catch (error) {
@@ -70,9 +127,133 @@ export default class FormMount extends React.Component {
             }
         }
     }
+    getFormStructure(){
+        if (this.state.selectedForm){
+            let camposSemTratar = this.state.selectedForm.FormsFields;
+        //camposSemTratar = this.state.selectedForm.FormsFields.map(field => 
+            let camposForm = []
+            for(const field of camposSemTratar ){
+                let campo;
+                if(field.NumericField){
+                    campo = new CampoNumerico(field.NumericField.min, field.NumericField.max);
+                } else if(field.TextField){
+                    campo = new CampoTexto(field.Textfield.char_limit)
+                } else if(field.OptionField){
+                    campo = new CampoOpcoes(1, 3, "", [])
+                }
+                let campoForm = new CampoFormulario(field.name, false, true, campo);
+                camposForm.push(campoForm);
+            }
+            let categoria = new Categoria(false, true, "Teste", camposForm);
+            return new EstruturaFormulario([categoria]);
+        }
+    }
+    getSchema (estruturaFormulario) {
+        let sqm = {
+            "title": "Avaliação da Praça X",
+            "type": "object",
+            "properties": []
+        }
+        for (const categoria of estruturaFormulario.categorias){
+            let prop = {
+                "type": "object",
+                "title": categoria.nome,
+                "properties": [],
+            }
+            for(let campo of categoria.campos){
+                if(campo.campo instanceof CampoTexto){
+                    let cmp = {
+                        type: "string",
+                        title: campo.nome,
+                    }
+                    prop.properties.push(cmp);
+                } else if( campo.campo instanceof CampoOpcoes){
+                    if(campo.campo.opcoes.length == 1){
+                        prop.properties.push({
+                            type: "boolean", 
+                            title: campo.nome,
+                        })
+                    }
+                    else{
+                        let options =[]
+                        for(let item of campo.campo.opcoes){
+                            options.push(item.nome)
+                        }
+                        prop.properties.push({
+                            type: "array",
+                            title: campo.nome,
+                            items: {
+                                type: "string",
+                                enum: options,
+                                      "uniqueItems": true
+
+                            }
+                        })
+
+                    }
+
+                } else if( campo.campo instanceof CampoNumerico){
+                    prop.properties.push({
+                                "type": "integer",
+                                "title": campo.nome,
+                                "minimum": campo.campo.min,
+                                "maximum": campo.campo.max,
+                    })
+
+                }
+            }
+            sqm.properties.push(prop);
+        }
+        return sqm;
+    }
+    uiSchema = {
+        "integer": {
+            "ui:widget": "range"
+        },
+        "depredacao":{
+            "nivelAbandono": {
+                "ui:widget": "radio",
+                "ui:options": {
+                    "inline": true
+                }
+            },
+            "nivelPixacao": {
+                "ui:widget": "radio",
+                "ui:options": {
+                    "inline": true
+                }
+            },
+        },
+        "acessoEEntorno": {
+            "estacionamentoMotos": {
+                "ui:widget": "updown"
+            },
+            "estacionamentoVeiculos": {
+                "ui:widget": "updown"
+            },
+            "placaComNome": {
+                "ui:widget": "updown"
+            },
+            "pontoDeTaxi": {
+                "ui:widget": "updown"
+            },
+            "pontoDeTransporte": {
+                "ui:widget": "updown"
+            }
+        },
+         "multipleChoicesList": {
+    "ui:widget": "checkboxes"
+         }
+  };
+    createForm(){
+        const estruturaFormulario = this.getFormStructure();
+        const formSchema = this.getSchema(estruturaFormulario);
+        return <Form schema={formSchema} uiSchema={this.uiSchema} formData={this.formData}/>
+    }
 
     render() {
         return (
+
             <GridContainer>
                 <GridItem xs={12}>
                     <InputLabel id="lbl-header" style={labelStyle}><h2>Formularios</h2></InputLabel>
@@ -97,35 +278,7 @@ export default class FormMount extends React.Component {
                 <GridItem xs={12}>
                     {this.state.selectedForm && <>
                         <h3><b>Formulario {this.state.selectedForm.id}</b></h3>
-
-                        {this.state.selectedForm.FormsFields.map(field => {
-                            return <article key={field.id}>
-                                <h4><b>Pergunta:</b> {field.name}</h4>
-                                <h4><b>Categoria:</b> {field.category_id}</h4>
-
-                                {field.TextField && <>
-                                    <h4><b>Tipo:</b> texto</h4>
-                                    <h5><b>Char limit:</b> {field.TextField.char_limit || '-'}</h5>
-                                </>}
-
-                                {field.NumericField && <>
-                                    <h4><b>Tipo:</b> numero</h4>
-                                    <h5><b>Min:</b>{field.NumericField.min}</h5>
-                                    <h5><b>Max:</b>{field.NumericField.max}</h5>
-                                </>}
-
-                                {field.OptionField && <>
-                                    <h4><b>Tipo:</b> opcao</h4>
-                                    <h5><b>Preferencia visual:</b> {field.OptionField.visual_preference}</h5>
-                                    <h5><b>Opcoes:</b></h5>
-                                    <ul>
-                                        {field.OptionField.Options.map(e => <li key={e.id}>{e.name || 'null'}</li>)}
-                                    </ul>
-                                </>}
-
-                                <Divider />
-                            </article>
-                        })}
+                        <> {this.createForm()} </>
                     </>
                     }
                 </GridItem>
