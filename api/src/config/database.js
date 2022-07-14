efe5700c1d0421cd42152c13baa995c84ba88362
FilipeBrinati <44785517+FilipeBrinati@users.noreply.github.com>
2022-07-14 16:34:30 -0300

    database changes

--- api/src/config/database.js
+++ api/src/config/database.js
@@ -2,8 +2,8 @@ module.exports = {
     dialect: 'postgres',
     host: 'localhost',
     username: 'postgres',
-    password: '306eE5066',
-    database: 'postgres',
+    password: '02Ago1962',
+    database: 'square_project',
     define: {
         timestamps: true,
         underscored: false,
