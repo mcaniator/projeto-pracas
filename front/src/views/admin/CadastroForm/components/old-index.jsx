72ba6d000f6bf1a848cc9948c337e98f415e1d2b
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2022-07-18 16:57:08 -0300

    accordion de categorias e perguntas

---
+++ front/src/views/admin/CadastroForm/components/old-index.jsx
@@ -0,0 +1,114 @@
+import React from "react";
+import GridContainer from "components/Grid/GridContainer";
+import GridItem from "components/Grid/GridItem";
+import Tabs from "components/CustomTabs/CustomTabs.js";
+import Question from "./Question.jsx";
+import FormGenerator from "./FormGenerator.jsx";
+import { FormTest } from './FormTest.jsx';
+
+import axios from "axios";
+import FormPicker from "./FormPicker.jsx";
+import FormMount from "./FormMount.jsx";
+
+export default class FormBuilder extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            categories: [],
+            fields: [],
+        };
+
+        this.insertQuestion = this.insertQuestion.bind(this);
+        this.insertCategory = this.insertCategory.bind(this);
+        this.init = this.init.bind(this);
+
+        this.init();
+    }
+
+    async init() {
+        try {
+            let categories = await axios.get('http://localhost:3333/category');
+            let fields = await axios.get('http://localhost:3333/form_field');
+
+            if (categories.status === 200) {
+                this.setState({
+                    categories: categories.data,
+                    fields: fields.data
+                })
+            }
+        } catch (e) {
+            console.error(e);
+        }
+    }
+
+    async insertQuestion(question, type, field, options) {
+        try {
+            let resForms = await axios.post('http://localhost:3333/form_field', { question, type, field, options })
+            console.log('resforms:', resForms);
+            console.log('data:', resForms.data);
+
+            this.setState({
+                fields: [...this.state.fields, resForms.data.formField]
+            })
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    async insertCategory(name) {
+        let newCategory = {
+            name,
+            optional: false,
+            active: true,
+        }
+
+        try {
+            let res = await axios.post('http://localhost:3333/category', { category: [newCategory] })
+
+            if (res.status == 200)
+                this.setState({
+                    categories: [...this.state.categories, ...res.data]
+                })
+
+        }
+        catch (e) {
+            console.error(e);
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <GridContainer>
+                    <GridItem xs={12}>
+                        <Tabs
+                            // title="Construtor de Formulario:"
+                            headerColor="success"
+                            tabs={[
+                                {
+                                    tabName: "1",
+                                    tabContent: <Question categories={this.state.categories} insertQuestion={this.insertQuestion} insertCategory={this.insertCategory} />
+                                },
+                                {
+                                    tabName: "2",
+                                    tabContent: <FormPicker categories={this.state.categories} fields={this.state.fields} axios={axios} />
+                                },
+                                {
+                                    tabName: "3",
+                                    tabContent: <FormTest categories={this.state.categories} fields={this.state.fields} />
+                                    // tabContent: <FormGenerator onDataChange={this.handleDataChange} />
+                                },
+                                {
+                                    tabName: "4",
+                                    tabContent: <FormMount categories={this.state.categories} axios={axios} />
+                                }
+                            ]}
+                        />
+                    </GridItem>
+                </GridContainer>
+            </div>
+        );
+    }
+}
--- front/src/views/admin/CadastroForm/index.jsx
+++ front/src/views/admin/CadastroForm/index.jsx
@@ -1,114 +1,62 @@
 import React from "react";
 import GridContainer from "components/Grid/GridContainer";
 import GridItem from "components/Grid/GridItem";
-import Tabs from "components/CustomTabs/CustomTabs.js";
-import Question from "./components/Question.jsx";
-import FormGenerator from "./components/FormGenerator.jsx";
-import { FormTest } from './components/FormTest.jsx';
-
 import axios from "axios";
-import FormPicker from "./components/FormPicker.jsx";
-import FormMount from "./components/FormMount.jsx";
+import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography, IconButton, ListItem, ListItemText, List, ListItemIcon, Button } from "@material-ui/core";
+import { Add, ExpandMore } from "@material-ui/icons";
 
 export default class FormBuilder extends React.Component {
     constructor(props) {
         super(props);
 
         this.state = {
-            categories: [],
-            fields: [],
-        };
+            data: [], // categoria com suas perguntas [{category, FormFields: []}]
+        }
 
-        this.insertQuestion = this.insertQuestion.bind(this);
-        this.insertCategory = this.insertCategory.bind(this);
         this.init = this.init.bind(this);
 
         this.init();
     }
 
     async init() {
-        try {
-            let categories = await axios.get('http://localhost:3333/category');
-            let fields = await axios.get('http://localhost:3333/form_field');
+        let response = await axios.get('http://localhost:3333/form_field/category');
 
-            if (categories.status === 200) {
-                this.setState({
-                    categories: categories.data,
-                    fields: fields.data
-                })
-            }
-        } catch (e) {
-            console.error(e);
-        }
-    }
-
-    async insertQuestion(question, type, field, options) {
-        try {
-            let resForms = await axios.post('http://localhost:3333/form_field', { question, type, field, options })
-            console.log('resforms:', resForms);
-            console.log('data:', resForms.data);
-
-            this.setState({
-                fields: [...this.state.fields, resForms.data.formField]
-            })
-        }
-        catch (e) {
-            console.error(e);
-        }
-    }
-
-    async insertCategory(name) {
-        let newCategory = {
-            name,
-            optional: false,
-            active: true,
-        }
-
-        try {
-            let res = await axios.post('http://localhost:3333/category', { category: [newCategory] })
-
-            if (res.status == 200)
-                this.setState({
-                    categories: [...this.state.categories, ...res.data]
-                })
-
-        }
-        catch (e) {
-            console.error(e);
-        }
+        this.setState({
+            data: response.data,
+        });
     }
 
     render() {
         return (
-            <div>
-                <GridContainer>
-                    <GridItem xs={12}>
-                        <Tabs
-                            // title="Construtor de Formulario:"
-                            headerColor="success"
-                            tabs={[
-                                {
-                                    tabName: "1",
-                                    tabContent: <Question categories={this.state.categories} insertQuestion={this.insertQuestion} insertCategory={this.insertCategory} />
-                                },
-                                {
-                                    tabName: "2",
-                                    tabContent: <FormPicker categories={this.state.categories} fields={this.state.fields} axios={axios} />
-                                },
-                                {
-                                    tabName: "3",
-                                    tabContent: <FormTest categories={this.state.categories} fields={this.state.fields} />
-                                    // tabContent: <FormGenerator onDataChange={this.handleDataChange} />
-                                },
-                                {
-                                    tabName: "4",
-                                    tabContent: <FormMount categories={this.state.categories} axios={axios} />
-                                }
-                            ]}
-                        />
-                    </GridItem>
-                </GridContainer>
-            </div>
-        );
+            <GridContainer>
+                <GridItem xs={12}><h2>Criar Formulario</h2></GridItem>
+                <GridItem xs={12} md={6} style={{ backgroundColor: '#aa0000' }}>
+                    {this.state.data.map(category => (
+                        <ExpansionPanel key={category.id}>
+                            <ExpansionPanelSummary expandIcon={<ExpandMore />}>
+                                <Typography>{category.name}</Typography>
+                            </ExpansionPanelSummary>
+                            <ExpansionPanelDetails>
+                                <List style={{ width: '100%', padding: 0 }}>
+                                    <ListItem>
+                                        <Button>Adcionar todas dessa categoria</Button>
+                                    </ListItem>
+
+                                    {category.FormsFields.map(field => (
+                                        <ListItem key={field.id}>
+                                            <ListItemText primary={field.name}></ListItemText>
+                                            <ListItemIcon><IconButton><Add /></IconButton></ListItemIcon>
+                                        </ListItem>
+                                    ))}
+                                </List>
+                            </ExpansionPanelDetails>
+                        </ExpansionPanel>
+                    )
+                    )}
+                </GridItem>
+                <GridItem xs={12} md={6} style={{ backgroundColor: '#00aa00' }}>
+                    tchau
+                </GridItem>
+            </GridContainer>);
     }
 }
