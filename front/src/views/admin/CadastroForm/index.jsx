929fb567921e0456fd38ed7a9acb4e43ac3eaaa4
Paulo Rozatto <rozatto.paulo.victor@gmail.com>
2022-07-18 18:15:14 -0300

    add categorias e campos como string

--- front/src/views/admin/CadastroForm/index.jsx
+++ front/src/views/admin/CadastroForm/index.jsx
@@ -5,12 +5,22 @@ import axios from "axios";
 import { ExpansionPanel, ExpansionPanelSummary, ExpansionPanelDetails, Typography, IconButton, ListItem, ListItemText, List, ListItemIcon, Button } from "@material-ui/core";
 import { Add, ExpandMore } from "@material-ui/icons";
 
+import Form from "@rjsf/material-ui";
+
+const formData = {};
 export default class FormBuilder extends React.Component {
     constructor(props) {
         super(props);
 
+
         this.state = {
             data: [], // categoria com suas perguntas [{category, FormFields: []}]
+            formSchema: {
+                title: "Criar Fom",
+                type: "object",
+                properties: {},
+            },
+            uiSchema: {}
         }
 
         this.init = this.init.bind(this);
@@ -26,12 +36,78 @@ export default class FormBuilder extends React.Component {
         });
     }
 
+    addCategory(idx, fields = {}) {
+        let category = this.state.data[idx];
+
+        if (category) {
+            this.setState({
+                formSchema: {
+                    ...this.state.formSchema,
+                    properties: {
+                        ...this.state.formSchema.properties,
+                        [category.name]: {
+                            type: 'object',
+                            properties: fields,
+                        }
+                    }
+                }
+            })
+        }
+    }
+
+    addAllFields(idx) {
+        let category = this.state.data[idx];
+
+        if (category) {
+            const fields = {}
+            for (let field of category.FormsFields) {
+                fields[field.name] = {
+                    type: 'string'
+                }
+            }
+            this.addCategory(idx, fields);
+        }
+    }
+
+    addField(catIdx, fieIdx) {
+        let category = this.state.data[catIdx];
+        let field = category.FormsFields[fieIdx];
+
+        if (!this.state.formSchema.properties[category.name]) {
+
+
+            this.addCategory(catIdx, {
+                [field.name]: {
+                    type: 'string'
+                }
+            });
+        }
+        else {
+            let stateCategory = this.state.formSchema.properties[category.name];
+
+            stateCategory.properties[field.name] = {
+                type: 'string'
+            }
+            console.log(stateCategory)
+
+            this.setState({
+                formSchema: {
+                    ...this.state.formSchema,
+                    properties: {
+                        ...this.state.formSchema.properties,
+                        [category.name]: stateCategory
+                    }
+                }
+            })
+        }
+    }
+
     render() {
         return (
             <GridContainer>
                 <GridItem xs={12}><h2>Criar Formulario</h2></GridItem>
                 <GridItem xs={12} md={6} style={{ backgroundColor: '#aa0000' }}>
-                    {this.state.data.map(category => (
+                    {this.state.data.map((category, catIdx) => (
                         <ExpansionPanel key={category.id}>
                             <ExpansionPanelSummary expandIcon={<ExpandMore />}>
                                 <Typography>{category.name}</Typography>
@@ -39,13 +115,13 @@ export default class FormBuilder extends React.Component {
                             <ExpansionPanelDetails>
                                 <List style={{ width: '100%', padding: 0 }}>
                                     <ListItem>
-                                        <Button>Adcionar todas dessa categoria</Button>
+                                        <Button onClick={() => { this.addAllFields(catIdx) }}>Adcionar todas dessa categoria</Button>
                                     </ListItem>
 
-                                    {category.FormsFields.map(field => (
+                                    {category.FormsFields.map((field, fieIdx) => (
                                         <ListItem key={field.id}>
                                             <ListItemText primary={field.name}></ListItemText>
-                                            <ListItemIcon><IconButton><Add /></IconButton></ListItemIcon>
+                                            <ListItemIcon><IconButton onClick={() => { this.addField(catIdx, fieIdx) }}><Add /></IconButton></ListItemIcon>
                                         </ListItem>
                                     ))}
                                 </List>
@@ -55,7 +131,7 @@ export default class FormBuilder extends React.Component {
                     )}
                 </GridItem>
                 <GridItem xs={12} md={6} style={{ backgroundColor: '#00aa00' }}>
-                    tchau
+                    < Form schema={this.state.formSchema} uiSchema={this.state.uiSchema} formData={formData} />
                 </GridItem>
             </GridContainer>);
     }
