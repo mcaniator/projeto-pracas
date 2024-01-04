dac9569cb3c92ce6ebcefad7aab10ae1374da0d6
Samuel Nascimento <professional@samnsc.com>
2024-01-03 21:52:22 -0300

    login button prettier formatting

--- src/components/loginButton.tsx
+++ src/components/loginButton.tsx
@@ -1,8 +1,4 @@
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
+import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { IconLogin } from "@tabler/icons-react";
 import Link from "next/link";
@@ -13,21 +9,15 @@ const LoginButton = () => {
   return (
     <Popover>
       <PopoverTrigger className="ml-auto">
-        <Button
-          asChild
-          variant={"ghost"}
-          className="flex items-center px-3 py-6 pl-2"
-        >
+        <Button asChild variant={"ghost"} className="flex items-center px-3 py-6 pl-2">
           <div>
             <IconLogin size={34} />
-            <span className="pointer-events-none text-2xl sm:text-3xl ">
-              Login
-            </span>
+            <span className="pointer-events-none text-2xl sm:text-3xl ">Login</span>
           </div>
         </Button>
       </PopoverTrigger>
 
-      <PopoverContent className="mr-7 rounded-2xl border-0 bg-white/20 backdrop-blur-lg z-[10000000]">
+      <PopoverContent className="z-[10000000] mr-7 rounded-2xl border-0 bg-white/20 backdrop-blur-lg">
         <Tabs defaultValue="login">
           <TabsList className="bg-transparent">
             <TabsTrigger value="login">
@@ -39,10 +29,7 @@ const LoginButton = () => {
           </TabsList>
 
           <TabsContent className="flex w-full rounded-lg" value="login">
-            <Button
-              variant={"ghost"}
-              className="bg-white/20 text-white shadow-lg"
-            >
+            <Button variant={"ghost"} className="bg-white/20 text-white shadow-lg">
               <Link href="/admin">Ir para admin</Link>
             </Button>
           </TabsContent>
