fed5bfecef612dc3a074e7fcf4feefc17e6fa81e
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-02-02 17:15:44 -0300

    Functional shpjs library

--- src/serverActions/getPolygonFromShp.ts
+++ src/serverActions/getPolygonFromShp.ts
@@ -1,9 +1,37 @@
 "use server";
 
+import { point } from "leaflet";
+//import { geoJSON } from "leaflet";
+//import shpjs from "shpjs";
+import shp from "shpjs";
+
 const getPolygonFromShp = async (formData: FormData) => {
-  const file = formData.get("shpFile") as File;
+  const file = formData.get("shpFile");
   const arrayBuffer = await file.arrayBuffer();
-  console.log(file);
+  const geoJSON = await shp(arrayBuffer);
+  console.log(geoJSON.features);
+
+  geoJSON.features.forEach((feature) => {
+    if (feature.geometry.type == "Polygon") {
+      feature.geometry.coordinates.forEach((pointsArray) => {
+        pointsArray.forEach((point) => {
+          console.log(point);
+        });
+      });
+    }
+  });
+
+  geoJSON.features[0].geometry.coordinates.forEach((pointsArray) => {
+    pointsArray.forEach((point) => {
+      //console.log(point);
+    });
+  });
+
+  /*geoJSON.features[0].geometry.coordinates[0].forEach((point) => {
+    //console.log(point);
+  });*/
+
+  //console.log(file);
   //const shapefile = require("shapefile");
   //const geo = shapefile.shp2json
 };
