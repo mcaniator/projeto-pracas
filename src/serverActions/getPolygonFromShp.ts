73c09b370866a7b50fd6a7f0fb29b9dfa602157f
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-02-16 00:40:13 -0300

    getPolygonFromShp transforms shp into WKT

--- src/serverActions/getPolygonFromShp.ts
+++ src/serverActions/getPolygonFromShp.ts
@@ -1,39 +1,21 @@
 "use server";
 
-import { point } from "leaflet";
-//import { geoJSON } from "leaflet";
-//import shpjs from "shpjs";
-import shp from "shpjs";
+import { parseShp } from "shpjs";
 
 const getPolygonFromShp = async (formData: FormData) => {
   const file = formData.get("shpFile");
   const arrayBuffer = await file.arrayBuffer();
-  const geoJSON = await shp(arrayBuffer);
-  console.log(geoJSON.features);
+  const geoJSON = parseShp(arrayBuffer);
 
-  geoJSON.features.forEach((feature) => {
-    if (feature.geometry.type == "Polygon") {
-      feature.geometry.coordinates.forEach((pointsArray) => {
-        pointsArray.forEach((point) => {
-          console.log(point);
-        });
-      });
-    }
-  });
+  const polygons = geoJSON.map(
+    (polygon) =>
+      `(${polygon.coordinates
+        .map((ring) => `(${ring.map((array) => array.join(" ")).join(", ")})`)
+        .join(",")})`,
+  );
 
-  geoJSON.features[0].geometry.coordinates.forEach((pointsArray) => {
-    pointsArray.forEach((point) => {
-      //console.log(point);
-    });
-  });
-
-  /*geoJSON.features[0].geometry.coordinates[0].forEach((point) => {
-    //console.log(point);
-  });*/
-
-  //console.log(file);
-  //const shapefile = require("shapefile");
-  //const geo = shapefile.shp2json
+  const polygonsString = `MULTIPOLYGON (${polygons.join(", ")})`;
+  console.log(polygonsString);
 };
 
 export { getPolygonFromShp };
