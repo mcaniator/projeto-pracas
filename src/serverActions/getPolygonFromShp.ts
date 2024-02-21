c3ffcda3878c008615e41aefae9ae6e24cd5d530
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-02-21 15:22:44 -0300

    Added geometry type check

--- src/serverActions/getPolygonFromShp.ts
+++ src/serverActions/getPolygonFromShp.ts
@@ -6,29 +6,36 @@ import { parseShp } from "shpjs";
 const getPolygonFromShp = async (shpFile: File) => {
   if (shpFile.name.toLowerCase().endsWith(".shp")) {
     const arrayBuffer: ArrayBuffer = await shpFile.arrayBuffer();
-
     const geometryArray: Geometry[] = parseShp(arrayBuffer);
-    const geometries = geometryArray.map((geometry: Geometry) => {
-      if (geometry.type === "Polygon") {
-        return `(${geometry.coordinates
-          .map(
-            (ring: Position[]) =>
-              `(${ring.map((array: Position) => array.slice(0, 2).join(" ")).join(", ")})`,
-          )
-          .join(",")})`;
-      } else if (geometry.type === "MultiPolygon") {
-        return geometry.coordinates.map((multipolygon: Position[][]) => {
-          const polygons = multipolygon.map(
-            (ring: Position[]) =>
-              `(${ring.map((array: Position) => array.slice(0, 2).join(" ")).join(", ")})`,
-          );
-          return `(${polygons.join(", ")})`;
-        });
-      }
-    });
-
-    const polygonsString: string = `MULTIPOLYGON (${geometries.join(", ")})`;
-    return polygonsString;
+    const geometriesWKT = geometryArray
+      .filter(
+        (geometry: Geometry) =>
+          geometry.type === "Polygon" || geometry.type === "MultiPolygon",
+      )
+      .map((geometry: Geometry) => {
+        if (geometry.type === "Polygon") {
+          return `(${geometry.coordinates
+            .map(
+              (ring: Position[]) =>
+                `(${ring.map((array: Position) => array.slice(0, 2).join(" ")).join(", ")})`,
+            )
+            .join(",")})`;
+        } else if (geometry.type === "MultiPolygon") {
+          return geometry.coordinates.map((multipolygon: Position[][]) => {
+            const polygons = multipolygon.map(
+              (ring: Position[]) =>
+                `(${ring.map((array: Position) => array.slice(0, 2).join(" ")).join(", ")})`,
+            );
+            return `(${polygons.join(", ")})`;
+          });
+        }
+      });
+    if (geometriesWKT.length > 0) {
+      const multipolygonWKT = `MULTIPOLYGON (${geometriesWKT.join(", ")})`;
+      return multipolygonWKT;
+    } else {
+      return null;
+    }
   } else {
     return null;
   }
