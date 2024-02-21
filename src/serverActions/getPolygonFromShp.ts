51be39fb3453bfd141556536d83e9452250f1b74
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-02-21 13:54:20 -0300

    Added support for multipolygons in geometry array

--- src/serverActions/getPolygonFromShp.ts
+++ src/serverActions/getPolygonFromShp.ts
@@ -7,8 +7,8 @@ const getPolygonFromShp = async (shpFile: File) => {
   if (shpFile.name.toLowerCase().endsWith(".shp")) {
     const arrayBuffer: ArrayBuffer = await shpFile.arrayBuffer();
 
-    const geoJSON: Geometry[] = parseShp(arrayBuffer);
-    const geometries = geoJSON.map((geometry: Geometry) => {
+    const geometryArray: Geometry[] = parseShp(arrayBuffer);
+    const geometries = geometryArray.map((geometry: Geometry) => {
       if (geometry.type === "Polygon") {
         return `(${geometry.coordinates
           .map(
@@ -16,11 +16,18 @@ const getPolygonFromShp = async (shpFile: File) => {
               `(${ring.map((array: Position) => array.slice(0, 2).join(" ")).join(", ")})`,
           )
           .join(",")})`;
+      } else if (geometry.type === "MultiPolygon") {
+        return geometry.coordinates.map((multipolygon: Position[][]) => {
+          const polygons = multipolygon.map(
+            (ring: Position[]) =>
+              `(${ring.map((array: Position) => array.slice(0, 2).join(" ")).join(", ")})`,
+          );
+          return `(${polygons.join(", ")})`;
+        });
       }
     });
 
     const polygonsString: string = `MULTIPOLYGON (${geometries.join(", ")})`;
-
     return polygonsString;
   } else {
     return null;
