778b077c225a19f05e9cd611e5c6ab5c82387b8a
Samuel Nascimento <professional@samnsc.com>
2024-03-10 11:19:50 -0300

    fix middleware issues

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,21 +3,20 @@ import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
 const middleware = (request: NextRequest) => {
-  if (request.nextUrl.pathname == "/admin") {
-    const url = request.nextUrl.clone();
-    url.pathname = "/admin/home";
-
-    return NextResponse.redirect(url);
-  }
-
-  if (request.nextUrl.pathname == "/admin/registration") {
-    const url = request.nextUrl.clone();
-    url.pathname = "/admin/registration/questions";
-
-    return NextResponse.redirect(url);
-  }
-
   if (request.method === "GET") {
+    const url = request.nextUrl.clone();
+
+    if (request.nextUrl.pathname === "/admin") {
+      url.pathname = "/admin/home";
+      return NextResponse.redirect(url);
+    }
+
+    if (request.nextUrl.pathname === "/admin/registration") {
+      const url = request.nextUrl.clone();
+      url.pathname = "/admin/registration/questions";
+      return NextResponse.redirect(url);
+    }
+
     return NextResponse.next();
   }
 
