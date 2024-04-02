8441414829348e1f1f48bc6a224ab829269d451e
Lucas Paiva <lucaspaiva@live.com>
2024-04-02 04:27:33 -0300

    refatora o frontend da adição de perguntas aos formulários

--- src/components/singleUse/admin/question/questionForm.tsx
+++ src/components/singleUse/admin/question/questionForm.tsx
@@ -2,36 +2,45 @@
 
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
-import { addQuestion } from "@/serverActions/formUtil";
+// import { addQuestion } from "@/serverActions/formUtil";
 import { searchQuestionsByStatement } from "@/serverActions/questionUtil";
 import { Form } from "@prisma/client";
 import { Suspense, use, useDeferredValue, useEffect, useState } from "react";
 
+const handleAddQuestion = (
+  questionId: number,
+  addToQuestionsToAddId: (id: number) => void,
+) => {
+  addToQuestionsToAddId(questionId);
+};
+
 const QuestionComponent = ({
-  id,
+  questionId,
+  addToQuestionsToAddId,
   name,
   formId,
 }: {
-  id: number;
+  questionId: number;
+  addToQuestionsToAddId: (id: number) => void;
   name: string;
   formId?: number;
 }) => {
   return (
     <div
-      key={id}
+      key={questionId}
       className="mb-2 flex items-center justify-between rounded bg-white p-2"
     >
       <Button
         variant={"admin"}
         type="submit"
         className={"w-min"}
-        onClick={() => void addQuestion(formId, id)}
+        onClick={() => handleAddQuestion(questionId, addToQuestionsToAddId)}
       >
         <span className={"-mb-1"}>Adicionar</span>
       </Button>
       {name}
       <div>o id do formulário é: {formId}</div>
-      <div>o id da pergunta é:{id}</div>
+      <div>o id da pergunta é:{questionId}</div>
     </div>
   );
 };
@@ -43,19 +52,30 @@ const QuestionList = ({
   questionPromise?: Promise<Form[]>;
   formId?: number;
 }) => {
+  const [questionsToAddId, setQuestionsToAddId] = useState<number[]>([]);
+  useEffect(() => {}, [questionsToAddId.length]);
   if (questionPromise === undefined) return null;
   const questions = use(questionPromise);
 
+  const addToQuestionsToAddId = (questionId: number) => {
+    if (!questionsToAddId.includes(questionId)) {
+      setQuestionsToAddId([...questionsToAddId, questionId]);
+    }
+  };
   return questions === undefined || questions.length === 0 ?
       null
     : <div className="w-full text-black">
+        <div className="text-xl">
+          id das perguntas a serem adicionadas é :{questionsToAddId}
+        </div>
         {questions.length > 0 &&
           questions.map((question) => (
             <QuestionComponent
               key={question.id}
-              id={question.id}
+              questionId={question.id}
               name={question.name}
               formId={formId}
+              addToQuestionsToAddId={addToQuestionsToAddId}
             />
           ))}
       </div>;
