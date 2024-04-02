7336fb58747a3d359f6b0511963af0a27cc7ba52
Lucas Paiva <lucaspaiva@live.com>
2024-04-02 02:41:16 -0300

    Correção de bug na tela de seleção de formulário para avaliação

--- src/app/admin/parks/[locationId]/evaluation/surveyQuestions.tsx
+++ src/app/admin/parks/[locationId]/evaluation/surveyQuestions.tsx
@@ -1,36 +1,40 @@
-"use client";
-
 import { SurveyResponsesComponent } from "@/components/singleUse/admin/registration/forms/surveyResponsesComponent";
 import { prisma } from "@/lib/prisma";
 import "@/serverActions/locationUtil";
 import { Form, Location } from "@prisma/client";
 
-const SurveyQuestions = async ({ location }: { location: Location }) => {
+const SurveyQuestions = ({ location }: { location: Location }) => {
   let forms: Form[];
-  try {
-    forms = await prisma.form.findMany();
-  } catch (e) {
-    // console.error(e);
-    forms = [];
-  }
-  return (
-    <div>
-      <div>Escolha o formulário da avaliação</div>
-      {forms !== null ?
-        <div className="w-full">
-          {forms.map((form) => (
-            <SurveyResponsesComponent
-              key={form.id}
-              selectedFormId={form.id}
-              name={form.name}
-              locationId={location.id}
-            />
-          ))}
+  return prisma.form
+    .findMany()
+    .then((foundForms) => {
+      forms = foundForms;
+      return (
+        <div>
+          <div>Escolha o formulário da avaliação</div>
+          {forms.length > 0 ?
+            <div className="w-full">
+              {forms.map((form) => (
+                <SurveyResponsesComponent
+                  key={form.id}
+                  selectedFormId={form.id}
+                  name={form.name}
+                  locationId={location.id}
+                />
+              ))}
+            </div>
+          : <div className="text-redwood">
+              Ainda não há perguntas no formulário
+            </div>
+          }
         </div>
-      : <div className="text-redwood">Ainda não há perguntas no formulário</div>
-      }
-    </div>
-  );
+      );
+    })
+    .catch((error) => {
+      console.error(error);
+      forms = [];
+      return <div className="text-redwood">Erro ao carregar formulários</div>;
+    });
 };
 
 export { SurveyQuestions };
