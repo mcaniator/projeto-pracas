1e3db19520bcfdbff9e1058113c70385febddf35
Samuel Nascimento <professional@samnsc.com>
2024-05-08 11:26:57 -0300

    remove unneeded dependencies

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -2,7 +2,7 @@
 
 import { cn } from "@/lib/cn";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
-import { X } from "lucide-react";
+import { IconX } from "@tabler/icons-react";
 import * as React from "react";
 
 const Dialog = DialogPrimitive.Root;
@@ -53,7 +53,7 @@ const DialogContent = React.forwardRef<
       {children}
       {disabledClose ? null : (
         <DialogPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity data-[state=open]:bg-accent data-[state=open]:text-muted-foreground hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none">
-          <X className="h-4 w-4" />
+          <IconX className="h-4 w-4" />
           <span className="sr-only">Close</span>
         </DialogPrimitive.Close>
       )}
