e00399655a2be2670d5171908f9c52db0de24bd3
Lucas Paiva <lucaspaiva@live.com>
2024-05-11 18:48:50 -0300

    refatora a busca de formulários na avaliação para encontrar somente os mais atuais

--- src/app/admin/parks/[locationId]/evaluation/surveyQuestions.tsx
+++ src/app/admin/parks/[locationId]/evaluation/surveyQuestions.tsx
@@ -1,40 +1,29 @@
 import { SurveyResponsesComponent } from "@/components/singleUse/admin/registration/forms/surveyResponsesComponent";
-import { prisma } from "@/lib/prisma";
+import { fetchFormsLatest } from "@/serverActions/formUtil";
 import "@/serverActions/locationUtil";
 import { Form, Location } from "@prisma/client";
 
-const SurveyQuestions = ({ location }: { location: Location }) => {
-  let forms: Form[];
-  return prisma.form
-    .findMany()
-    .then((foundForms) => {
-      forms = foundForms;
-      return (
-        <div>
-          <div>Escolha o formulário da avaliação</div>
-          {forms.length > 0 ?
-            <div className="w-full">
-              {forms.map((form) => (
-                <SurveyResponsesComponent
-                  key={form.id}
-                  selectedFormId={form.id}
-                  name={form.name}
-                  locationId={location.id}
-                />
-              ))}
-            </div>
-          : <div className="text-redwood">
-              Ainda não há perguntas no formulário
-            </div>
-          }
+const SurveyQuestions = async ({ location }: { location: Location }) => {
+  const forms: Form[] = await fetchFormsLatest();
+
+  return (
+    <div>
+      <div>Escolha o formulário da avaliação</div>
+      {forms.length > 0 ?
+        <div className="w-full">
+          {forms.map((form) => (
+            <SurveyResponsesComponent
+              key={form.id}
+              selectedFormId={form.id}
+              name={form.name}
+              locationId={location.id}
+            />
+          ))}
         </div>
-      );
-    })
-    .catch((error) => {
-      console.error(error);
-      forms = [];
-      return <div className="text-redwood">Erro ao carregar formulários</div>;
-    });
+      : <div className="text-redwood">Ainda não há perguntas no formulário</div>
+      }
+    </div>
+  );
 };
 
 export { SurveyQuestions };
