b9db88df898a41e3535347d7985c38c4ff215807
Samuel Nascimento <professional@samnsc.com>
2024-06-06 11:20:56 -0300

    create input and formInput components

---
+++ src/components/formInput.tsx
@@ -0,0 +1,60 @@
+"use client";
+
+import { MutableRefObject } from "react";
+
+import { Input } from "./input";
+
+type errorValues<Type> = {
+  [Propery in keyof Type]?: string[] | undefined;
+};
+
+const FormInput = <
+  Type extends
+    | { [key: string]: string }
+    | { [key: string]: string | undefined },
+>({
+  objectKey,
+  answerValues,
+  errorValues,
+  checker,
+  label,
+  description,
+}: {
+  objectKey: keyof Type;
+  answerValues: MutableRefObject<Type>;
+  errorValues: errorValues<Type> | null;
+  checker: (key: keyof Type) => void;
+  label: string;
+  description?: string;
+}) => {
+  return (
+    <Input
+      name={objectKey as string}
+      label={label}
+      description={description}
+      defaultValue={answerValues.current[objectKey]}
+      onChange={(val) => {
+        // @ts-expect-error not sure what TS is complaining about here, but doesn't seem to be an actual issue
+        answerValues.current[objectKey] = val === "" ? undefined : val;
+        checker(objectKey);
+      }}
+      errorMessage={() => {
+        if (errorValues === null) return "";
+
+        const errors = errorValues[objectKey];
+        if (errors === undefined) return "";
+
+        const value = errors[0];
+        if (value === undefined) return "";
+
+        return value;
+      }}
+      isInvalid={
+        errorValues === null ? false : errorValues[objectKey] !== undefined
+      }
+      onBlur={() => checker(objectKey)}
+    />
+  );
+};
+
+export { FormInput };
