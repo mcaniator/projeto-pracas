faab2bfce5d938374ec107cfdf46358730ea64f7
Samuel Nascimento <professional@samnsc.com>
2024-06-06 11:23:24 -0300

    enable react strict mode

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
   webpack: (config) => {
     config.externals.push("@node-rs/argon2", "@node-rs/bcrypt");
     return config;
