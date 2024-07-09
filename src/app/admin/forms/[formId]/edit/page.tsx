921eaa3d1bb1a07cf700c2ca96cd4401ab6605d6
Lucas <lucaspaiva@live.com>
2024-07-08 21:10:57 -0300

    correção da busca de perguntas por enunciado

--- src/app/admin/forms/[formId]/edit/page.tsx
+++ src/app/admin/forms/[formId]/edit/page.tsx
@@ -5,7 +5,7 @@ import Client from "./client";
 
 const Edit = async ({ params }: { params: { formId: string } }) => {
   const form = await searchFormsById(parseInt(params.formId));
-  const questions = await searchQuestionsByFormId(parseInt(params.formId));
+  const questions = await searchQuestionsByFormId(parseInt(params.formId)) ?? [];
   // TODO: add error handling
   return <Client form={form} questions={questions} />;
 };
