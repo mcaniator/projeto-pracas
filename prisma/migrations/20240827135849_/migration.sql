1b05d5c7e6b181c4fb99e152829e82d69149f82f
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-10-24 16:28:43 -0300

    Squash prisma migrations

--- prisma/migrations/20240827135849_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `form_version` on the `response` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "response" DROP COLUMN "form_version";
--- prisma/migrations/20240906173010_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `type` to the `calculation` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "calculation" ADD COLUMN     "type" "calculation_types" NOT NULL;
