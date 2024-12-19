f2816ce2cff74a8d5ec8e18e9f849a2bbb2cadf6
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2024-12-19 11:12:32 -0300

    Removes hover from subcategory select

--- src/app/admin/registration/questions/subcategorySelect.tsx
+++ src/app/admin/registration/questions/subcategorySelect.tsx
@@ -43,14 +43,14 @@ const SubcategorySelect = ({
         {subcategories.length > 0 && (
           <button
             value="ALL"
-            className={`text-md rounded-t-xl p-2 sm:text-xl ${activeButton === "ALL" ? "bg-gray-900/50" : "bg-gray-900/20"} hover:bg-gray-900/40`}
+            className={`text-md rounded-t-xl p-2 sm:text-xl ${activeButton === "ALL" ? "bg-gray-900/50" : "bg-gray-900/20"} bg-gray-900/40`}
             onClick={() => onChange("ALL")}
           >
             TODAS
           </button>
         )}
         <button
-          className={`text-md rounded-t-xl p-2 sm:text-xl ${activeButton === "NULL" ? "bg-gray-900/50" : "bg-gray-900/20"} hover:bg-gray-900/40`}
+          className={`text-md rounded-t-xl p-2 sm:text-xl ${activeButton === "NULL" ? "bg-gray-900/50" : "bg-gray-900/20"} bg-gray-900/40`}
           onClick={() => onChange("NULL")}
         >
           NENHUMA
@@ -58,7 +58,7 @@ const SubcategorySelect = ({
         {subcategories.map((subcategory) => {
           return (
             <button
-              className={`text-md rounded-t-xl p-2 sm:text-xl ${activeButton === subcategory.id ? "bg-gray-900/50" : "bg-gray-900/20"} hover:bg-gray-900/40`}
+              className={`text-md rounded-t-xl p-2 sm:text-xl ${activeButton === subcategory.id ? "bg-gray-900/50" : "bg-gray-900/20"} bg-gray-900/40`}
               key={subcategory.id}
               onClick={() => onChange(subcategory.id)}
             >
