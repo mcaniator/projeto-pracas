389ce8520a51dc5dfa5cf4b827e98af82358b164
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2025-01-14 15:37:21 -0300

    Adds responsiveness to park search page

--- src/app/admin/parks/page.tsx
+++ src/app/admin/parks/page.tsx
@@ -7,17 +7,15 @@ const AdminRoot = async () => {
   });
 
   return (
-    <div>
-      <div className={"flex min-h-0 flex-grow gap-5 p-5"}>
-        <div className="flex basis-full flex-col gap-5 text-white">
-          <div
-            className={
-              "flex basis-1/5 flex-col gap-1 rounded-3xl bg-gray-300/30 p-3 shadow-md"
-            }
-          >
-            <h3 className={"text-2xl font-semibold"}>Busca de Locais</h3>
-            <ParkForm location={parkNames} />
-          </div>
+    <div className={"flex min-h-0 max-w-full flex-grow gap-5 p-5"}>
+      <div className="flex max-w-full basis-full flex-col gap-5 text-white">
+        <div
+          className={
+            "flex basis-1/5 flex-col gap-1 rounded-3xl bg-gray-300/30 p-3 shadow-md"
+          }
+        >
+          <h3 className={"text-2xl font-semibold"}>Busca de Locais</h3>
+          <ParkForm location={parkNames} />
         </div>
       </div>
     </div>
