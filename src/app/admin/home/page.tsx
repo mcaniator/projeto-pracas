db480d98676f537a3ae36596d47c219e57ac7556
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2025-02-03 08:06:46 -0300

    Updates home page

--- src/app/admin/home/page.tsx
+++ src/app/admin/home/page.tsx
@@ -1,11 +1,57 @@
 "use server";
 
+import {
+  IconHome,
+  IconListCheck,
+  IconLogs,
+  IconMap,
+  IconTableExport,
+} from "@tabler/icons-react";
+import Link from "next/link";
+
 const AdminRoot = () => {
   return (
-    <div className={"flex h-full flex-col text-white"}>
-      <h2 className="p-2 text-2xl font-semibold">
+    <div className={"flex h-full flex-col items-center text-white"}>
+      <h2 className="p-2 text-4xl font-semibold">
         Bem vindo(a) ao Projeto praças
       </h2>
+      <div className="mt-32 flex flex-wrap justify-center gap-2">
+        <Link
+          href="parks"
+          className="flex w-64 items-center justify-center rounded-lg bg-true-blue p-4 text-3xl bg-blend-darken shadow-md transition-all duration-200 hover:bg-indigo-dye"
+        >
+          <IconHome className="mb-1" size={34} />
+          Praças
+        </Link>
+        <Link
+          href="map"
+          className="flex w-64 items-center justify-center rounded-lg bg-true-blue p-4 text-3xl bg-blend-darken shadow-md transition-all duration-200 hover:bg-indigo-dye"
+        >
+          <IconMap className="mb-1" size={34} />
+          Mapa
+        </Link>
+        <Link
+          href="registration"
+          className="flex w-64 items-center justify-center rounded-lg bg-true-blue p-4 text-3xl bg-blend-darken shadow-md transition-all duration-200 hover:bg-indigo-dye"
+        >
+          <IconListCheck className="mb-1" size={34} />
+          Formulários
+        </Link>
+        <Link
+          href="registration"
+          className="flex w-64 items-center justify-center rounded-lg bg-true-blue p-4 text-3xl bg-blend-darken shadow-md transition-all duration-200 hover:bg-indigo-dye"
+        >
+          <IconTableExport className="mb-1" size={34} />
+          Exportar
+        </Link>
+        <Link
+          href="activity"
+          className="flex w-64 items-center justify-center rounded-lg bg-true-blue p-4 text-3xl bg-blend-darken shadow-md transition-all duration-200 hover:bg-indigo-dye"
+        >
+          <IconLogs className="mb-1" size={34} />
+          Atividade
+        </Link>
+      </div>
     </div>
   );
 };
