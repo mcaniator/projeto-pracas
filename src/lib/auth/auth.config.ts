0b23cb7243176eaa67838f32d1d80f44c0d4231c
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2025-06-23 17:35:33 -0300

    fix authjs trustHost in middleware

--- src/lib/auth/auth.config.ts
+++ src/lib/auth/auth.config.ts
@@ -9,6 +9,7 @@ import { userLoginSchema } from "../zodValidators";
 //We need to define session callback in auth.config.ts, and jwt callback in auth.ts. Check: https://github.com/nextauthjs/next-auth/issues/9836#issuecomment-2451288724
 export default {
   session: { strategy: "jwt", maxAge: 2592000 * 6 },
+  trustHost: true,
   pages: {
     error: "/auth/error",
     signIn: "/auth/login",
