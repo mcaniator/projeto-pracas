abef4f0ff1ba763c6bc86768bd628a36b09cd456
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2025-09-11 16:39:25 -0300

    feat(sorted-questions): adds CDialog, CToggleButtonGroup, and updates CButton

--- src/components/ui/cButton.tsx
+++ src/components/ui/cButton.tsx
@@ -1,13 +1,30 @@
 import Button, { ButtonProps } from "@mui/material/Button";
 import React from "react";
 
-type CButtonProps = ButtonProps;
+type CButtonProps = ButtonProps & {
+  dense?: boolean;
+  disableMinWidth?: boolean;
+};
 
 function CButton(props: CButtonProps) {
-  const { children, variant = "contained", ...rest } = props;
+  const {
+    children,
+    variant = "contained",
+    disableMinWidth,
+    dense,
+    ...rest
+  } = props;
+  const minWidthSx =
+    disableMinWidth ? { minWidth: "0px" } : { minWidth: "64px" };
+  const denseSx = dense ? { padding: "0px 0px", minWidth: "0px" } : {};
 
   return (
-    <Button color="primary" variant={variant} {...rest}>
+    <Button
+      color="primary"
+      variant={variant}
+      sx={{ ...minWidthSx, ...denseSx, ...props.sx }}
+      {...rest}
+    >
       {children}
     </Button>
   );
