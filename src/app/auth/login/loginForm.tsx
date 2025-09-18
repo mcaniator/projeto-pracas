1b19ea770c954f60323061a437781a7b78c892b8
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2025-09-18 02:11:59 -0300

    add front-end responsiviness 1.0 to login page

--- src/app/auth/login/loginForm.tsx
+++ src/app/auth/login/loginForm.tsx
@@ -9,6 +9,7 @@ import { Input } from "@components/ui/input";
 import _login from "@serverActions/login";
 import { IconTree } from "@tabler/icons-react";
 import { useActionState, useEffect } from "react";
+import Link from "next/link";
 
 const LoginForm = ({ enableGoogleLogin }: { enableGoogleLogin: boolean }) => {
   const { setHelperCard } = useHelperCard();
@@ -27,42 +28,48 @@ const LoginForm = ({ enableGoogleLogin }: { enableGoogleLogin: boolean }) => {
       ),
     });
   }, [state, setHelperCard]);
+
   return (
-    <div className="flex h-screen items-center justify-center bg-gradient-to-br from-cambridge-blue to-asparagus">
-      <div className="absolute w-full h-full">
-        <img src="/loginWave.svg"></img>
+    <div className="flex min-h-screen items-center justify-center bg-gradient-to-br from-cambridge-blue to-asparagus">
+      
+      <div className="absolute left-0 mt-[20em] lg:mt-0 top-0 h-full w-full ">
+        <img src="/loginWave.svg" className="h-full w-full object-cover" alt="" />
       </div>
-      <div className="flex flex-col pt-20 gap-12 w-1/2 h-full items-center justify-center z-10">
-        <img className="max-w-[600px]" src="/loginPraca.svg" alt="Ilustração de uma praça para a página de login"></img>
+
+      <div className="hidden lg:flex lg:w-1/2 flex-col pt-20 gap-12 h-full items-center justify-center z-10">
+        <img className="max-w-[600px]" src="/loginPraca.svg" alt="Ilustração de uma praça para a página de login" />
         <div className="flex gap-4 text-asparagus">
-          <IconTree size={56} className="inline" />
-          <h1 className="inline text-5xl">Projeto praças</h1>
+          <Link className="flex items-center transition-transform duration-300 ease-in-out hover:scale-105" href={"/"}>
+            <IconTree size={56} className="inline" />
+            <h1 className="inline text-5xl">Projeto praças</h1>
+          </Link>
         </div>
       </div>
+
       {isPending && <LoadingIcon className="h-32 w-32" />}
       {!isPending && (
-        <div className="z-10 flex flex-col gap-4 w-1/2 h-full p-6 text-center justify-center items-center">
-          <form action={formAction}>
+        <div className="z-10 flex w-full lg:w-1/2 flex-col gap-4 h-full p-8 text-center justify-center items-center">
+          <form action={formAction} className="w-full max-w-xs">
             <div className="flex flex-col gap-4 text-center text-white">
               <h2 className="text-2xl">Login</h2>
               <div className="text-left">
                 <label htmlFor="email">E-mail</label>
-                <Input className="bg-praca-green-dark border-none rounded-full w-lg" name="email" id="email" />
+                <Input className="bg-praca-green-dark border-none rounded-full w-full" name="email" id="email" />
               </div>
               <div className="text-left text-white">
                 <label htmlFor="password">Senha</label>
-                <Input className="border-none bg-praca-green-dark rounded-full" type="password" name="password" id="password" />
+                <Input className="border-none bg-praca-green-dark rounded-full w-full" type="password" name="password" id="password" />
                 <div className="text-right">
                   <ButtonLink
-                  href="/auth/requestPasswordRecovery"
-                  variant={"ghost"}
-                  className="text-sm text-black-500 text-right"
-                >
-                  Esqueci minha senha
-                </ButtonLink>
+                    href="/auth/requestPasswordRecovery"
+                    variant={"ghost"}
+                    className="text-sm text-black-500 text-right"
+                  >
+                    Esqueci minha senha
+                  </ButtonLink>
                 </div>
               </div>
-              <Button type="submit" variant={"constructive"}>
+              <Button type="submit" variant={"constructive"} className="transition-transform duration-300 ease-in-out hover:scale-105 cursor-pointer">
                 Entrar
               </Button>
             </div>
