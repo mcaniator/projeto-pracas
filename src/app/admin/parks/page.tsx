a6fc79dd414aa96cdba71c4a7082558cf3f2823a
Duda Nunes <eduarda.nunes@estudante.ufjf.br>
2025-10-04 01:53:03 -0300

    Fix BD Prisma Bug + Front End Parks Page

--- src/app/admin/parks/page.tsx
+++ src/app/admin/parks/page.tsx
@@ -11,24 +11,26 @@ const AdminRoot = async () => {
   });
 
   return (
-    <div className={"flex h-full max-w-full flex-grow gap-5 overflow-auto"}>
-      <div className="flex max-w-full basis-full flex-col gap-5 overflow-auto">
+    <div className={"flex justify-center h-full max-w-full flex-grow gap-5 overflow-auto bg-gradient-to-br from-cambridge-blue to-asparagus"}>
+      <div className="flex w-1/2 flex-col gap-5 overflow-auto">
         <div
           className={
-            "flex flex-col gap-1 overflow-auto rounded-3xl bg-gray-300/30 p-3 shadow-md"
+            "flex text-center flex-col gap-1 overflow-auto rounded-3xl bg-gray-300/20 p-3 shadow-md"
           }
         >
-          <h3 className={"text-2xl font-semibold"}>Locais</h3>
+          <h3 className={"text-5xl font-semibold"}>Locais</h3>
+
+          <ParkForm location={parkNames} />
+
           <PermissionGuard requiresAnyRoles={["PARK_MANAGER"]}>
             <Link
               href={"/admin/parks/locationRegister"}
-              className="flex w-fit items-center justify-center rounded-lg bg-emerald p-2 text-xl bg-blend-darken shadow-md transition-all duration-200 hover:bg-sea-green"
+              className="flex items-center justify-center border-none rounded-full bg-praca-green-dark p-2 text-xl bg-blend-darken shadow-md transition-all duration-200 hover:bg-praca-green-dark/85"
             >
               <IconCirclePlus />
             </Link>
           </PermissionGuard>
 
-          <ParkForm location={parkNames} />
         </div>
       </div>
     </div>
