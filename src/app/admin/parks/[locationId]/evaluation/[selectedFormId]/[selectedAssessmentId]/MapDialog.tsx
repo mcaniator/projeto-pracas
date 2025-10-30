ec1a6da3dc41f9af7268a31b5e107a015942fc87
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2025-10-30 15:59:57 -0300

    fix(sorted-questions): dynamically import map provider

--- src/app/admin/parks/[locationId]/evaluation/[selectedFormId]/[selectedAssessmentId]/MapDialog.tsx
+++ src/app/admin/parks/[locationId]/evaluation/[selectedFormId]/[selectedAssessmentId]/MapDialog.tsx
@@ -2,11 +2,15 @@
 
 import { QuestionGeometryTypes } from "@prisma/client";
 import { IconCheck, IconTrash } from "@tabler/icons-react";
+import dynamic from "next/dynamic";
 import { useRef, useState } from "react";
 
 import CDialog from "../../../../../../../components/ui/dialog/cDialog";
 import { ResponseGeometry } from "../../../../../../../lib/types/assessments/geometry";
-import MapProvider from "./MapProvider";
+
+const MapProvider = dynamic(() => import("./MapProvider"), {
+  ssr: false,
+});
 
 const MapDialog = ({
   openMapDialog,
