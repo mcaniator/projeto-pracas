eb376afe1d140ab1e5a4869d9bda785f78d911c4
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2026-01-03 19:38:11 -0300

    fix(sorted-questions): remove old map components

--- src/app/admin/map/map.tsx
+++ src/app/admin/map/map.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import LoadingIcon from "@components/LoadingIcon";
 import dynamic from "next/dynamic";
-import { Suspense } from "react";
 
 import PolygonsAndClientContainer from "./PolygonsAndClientContainer";
 
@@ -13,16 +11,7 @@ const MapProvider = dynamic(() => import("./mapProvider"), {
 const Map = () => {
   return (
     <MapProvider>
-      <Suspense
-        fallback={
-          <div className="fixed left-0 top-0 z-50 flex h-full w-full flex-col items-center justify-center bg-black bg-opacity-50 text-lg">
-            <LoadingIcon size={32} />
-            Carregando filtros...
-          </div>
-        }
-      >
-        <PolygonsAndClientContainer />
-      </Suspense>
+      <PolygonsAndClientContainer />
     </MapProvider>
   );
 };
--- src/app/admin/map/page.tsx
+++ src/app/admin/map/page.tsx
@@ -1,20 +1,7 @@
-import { Suspense } from "react";
-
-import LoadingIcon from "../../../components/LoadingIcon";
 import Map from "./map";
 
 const Page = () => {
-  return (
-    <Suspense
-      fallback={
-        <div className="flex justify-center">
-          <LoadingIcon size={32} />
-        </div>
-      }
-    >
-      <Map />
-    </Suspense>
-  );
+  return <Map />;
 };
 
 export default Page;
