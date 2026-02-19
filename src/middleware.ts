a7e0282c797a601915fc878d21fae8d7e39b410f
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2026-02-19 01:45:11 -0300

    fix(sorted-questions): adds redirect to admin

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,11 +4,19 @@ import authConfig from "./lib/auth/auth.config";
 
 const { auth } = NextAuth(authConfig);
 export default auth((req) => {
+  const pathname = req.nextUrl.pathname;
+  const search = req.nextUrl.search;
+
+  if (pathname === "/") {
+    const url = new URL("/admin/map", req.url);
+    url.search = search;
+    return Response.redirect(url);
+  }
+
+  // Auth required for all /admin routes
   if (!req.auth) {
     return Response.redirect(new URL("/auth/login", req.url));
   }
-  const pathname = req.nextUrl.pathname;
-  const search = req.nextUrl.search;
 
   if (pathname === "/admin") {
     const url = new URL("/admin/map", req.url);
@@ -18,5 +26,5 @@ export default auth((req) => {
 });
 
 export const config = {
-  matcher: ["/admin/:path*"],
+  matcher: ["/", "/admin/:path*"],
 };
