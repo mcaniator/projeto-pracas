50787e68245b4d8793759f3b38868fcbff4ef46b
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2026-05-21 23:25:11 -0300

    feat(form-preview): add CDatePicker

---
+++ src/components/ui/cDatePicker.tsx
@@ -0,0 +1,130 @@
+import {
+  DateValidationError,
+  MobileDatePicker,
+  MobileDatePickerProps,
+  PickerChangeHandlerContext,
+} from "@mui/x-date-pickers";
+import { PickerValue } from "@mui/x-date-pickers/internals";
+import React, { useCallback } from "react";
+
+type CDatePickerProps = MobileDatePickerProps & {
+  error?: boolean;
+  helperText?: string;
+  debounce?: number;
+  clearable?: boolean;
+};
+
+const CDatePicker = React.forwardRef<HTMLInputElement, CDatePickerProps>(
+  (props, ref) => {
+    const { debounce, clearable, helperText, onAccept, onChange, ...rest } =
+      props;
+
+    const timeoutRef = React.useRef<NodeJS.Timeout | null>(null);
+
+    const handleChange = useCallback(
+      (
+        newValue: PickerValue,
+        context: PickerChangeHandlerContext<DateValidationError>,
+      ) => {
+        if (!onChange) return;
+
+        if (!debounce || debounce <= 0) {
+          onChange(newValue, context);
+          return;
+        }
+        if (timeoutRef.current) clearTimeout(timeoutRef.current);
+
+        timeoutRef.current = setTimeout(() => {
+          onChange(newValue, context);
+        }, debounce);
+      },
+      [onChange, debounce],
+    );
+
+    const handleAccept = useCallback(
+      (
+        newValue: PickerValue,
+        context: PickerChangeHandlerContext<DateValidationError>,
+      ) => {
+        if (!onAccept) return;
+
+        if (!debounce || debounce <= 0) {
+          onAccept(newValue, context);
+          return;
+        }
+        if (timeoutRef.current) clearTimeout(timeoutRef.current);
+
+        timeoutRef.current = setTimeout(() => {
+          onAccept(newValue, context);
+        }, debounce);
+      },
+      [onAccept, debounce],
+    );
+
+    const fieldsetSx =
+      props.error ?
+        {
+          borderColor: "error.main",
+        }
+      : {};
+    const labelSx =
+      props.error ?
+        {
+          color: "error.main",
+        }
+      : {};
+    return (
+      <MobileDatePicker
+        ref={ref}
+        onChange={handleChange}
+        onAccept={handleAccept}
+        slotProps={{
+          field: {
+            clearable: clearable,
+          },
+          textField: {
+            helperText: helperText,
+            InputLabelProps: { shrink: true },
+            sx: {
+              mt: "8px",
+              "& .MuiPickersOutlinedInput-root": {
+                borderRadius: 6,
+                "& fieldset": fieldsetSx,
+                fontSize: 16,
+                "@media (min-width:680px)": {
+                  fontSize: 20,
+                },
+              },
+              "& .MuiInputLabel-root.Mui-focused": {
+                color: "primary.main",
+              },
+              "& .MuiInputLabel-root": labelSx,
+              "& fieldset legend": {
+                display: "none",
+              },
+              "& fieldset": {
+                backgroundColor: "white",
+              },
+              "& .MuiPickersSectionList-root": {
+                zIndex: 1,
+                pt: "12px",
+                pb: "0px",
+              },
+              "& .MuiInputAdornment-root": {
+                zIndex: 1,
+              },
+              "& .MuiPickersInputBase-root": {
+                borderRadius: "16px",
+              },
+            },
+          },
+        }}
+        {...rest}
+      />
+    );
+  },
+);
+
+CDatePicker.displayName = "CDatePicker";
+
+export default CDatePicker;
