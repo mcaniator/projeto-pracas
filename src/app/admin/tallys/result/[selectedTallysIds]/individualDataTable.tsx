f402c2bed0cb6d69b149840996f3698a2e3ff7e5
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2026-07-21 21:28:07 -0300

    fix(capacitor): fix tally result page crash

--- src/app/admin/tallys/result/[selectedTallysIds]/individualDataTable.tsx
+++ src/app/admin/tallys/result/[selectedTallysIds]/individualDataTable.tsx
@@ -26,66 +26,70 @@ const IndividualDataTable = ({ tallys }: { tallys: FinalizedTally[] }) => {
   return (
     <>
       <div className="flex flex-col gap-5 overflow-auto rounded">
-        {tallys.map((tally, key) => (
-          <div
-            key={key}
-            className="rounded border border-black p-4 xl:border-gray-300 xl:shadow-sm"
-          >
-            <p>
-              <strong>{"Data: "}</strong>
-              {dateFormatter.format(tally.startDate.getTime())}
-            </p>
-            <p>
-              <strong>{"Horário: "}</strong>
-              {hourFormatter.format(tally.startDate.getTime())}
-            </p>
-            <p>
-              <strong>{"Duração(mm:ss): "}</strong>
-              {tally.endDate ?
-                `${String(
-                  Math.floor(
-                    (tally.endDate?.getTime() - tally.startDate.getTime()) /
-                      (1000 * 60 * 60),
-                  ),
-                ).padStart(2, "0")}:${String(
-                  Math.floor(
-                    ((tally.endDate?.getTime() - tally.startDate.getTime()) %
-                      (1000 * 60 * 60)) /
-                      (1000 * 60),
-                  ),
-                ).padStart(2, "0")}`
-              : "Em andamento"}
-            </p>
-            <p>
-              <strong>{"Observador(a): "}</strong>
-              {formatName(tally.user.username ?? "")}
-            </p>
-            <p>
-              <strong>{"Temperatura: "}</strong>
-              {tally.temperature}°C
-            </p>
-            <p>
-              <strong>{"Condição climática: "}</strong>
-              {tally.weatherCondition ?
-                weatherConditionsMap.get(tally.weatherCondition)
-              : ""}
-            </p>
-            <span className="flex flex-wrap items-center">
-              <span className="w-fit">
-                <strong>{"Editar contagem: "}</strong>
+        {tallys.map((tally, key) => {
+          const startDate = new Date(tally.startDate);
+          const endDate = tally.endDate ? new Date(tally.endDate) : null;
+          return (
+            <div
+              key={key}
+              className="rounded border border-black p-4 xl:border-gray-300 xl:shadow-sm"
+            >
+              <p>
+                <strong>{"Data: "}</strong>
+                {dateFormatter.format(startDate)}
+              </p>
+              <p>
+                <strong>{"Horário: "}</strong>
+                {hourFormatter.format(startDate)}
+              </p>
+              <p>
+                <strong>{"Duração(mm:ss): "}</strong>
+                {endDate ?
+                  `${String(
+                    Math.floor(
+                      (endDate?.getTime() - startDate.getTime()) /
+                        (1000 * 60 * 60),
+                    ),
+                  ).padStart(2, "0")}:${String(
+                    Math.floor(
+                      ((endDate?.getTime() - startDate.getTime()) %
+                        (1000 * 60 * 60)) /
+                        (1000 * 60),
+                    ),
+                  ).padStart(2, "0")}`
+                : "Em andamento"}
+              </p>
+              <p>
+                <strong>{"Observador(a): "}</strong>
+                {formatName(tally.user.username ?? "")}
+              </p>
+              <p>
+                <strong>{"Temperatura: "}</strong>
+                {tally.temperature}°C
+              </p>
+              <p>
+                <strong>{"Condição climática: "}</strong>
+                {tally.weatherCondition ?
+                  weatherConditionsMap.get(tally.weatherCondition)
+                : ""}
+              </p>
+              <span className="flex flex-wrap items-center">
+                <span className="w-fit">
+                  <strong>{"Editar contagem: "}</strong>
+                </span>
+                <CButton
+                  square
+                  className="ml-2 w-fit"
+                  onClick={() => {
+                    navigateToTallyFillingPage(tally.id);
+                  }}
+                >
+                  <IconPencil />
+                </CButton>
               </span>
-              <CButton
-                square
-                className="ml-2 w-fit"
-                onClick={() => {
-                  navigateToTallyFillingPage(tally.id);
-                }}
-              >
-                <IconPencil />
-              </CButton>
-            </span>
-          </div>
-        ))}
+            </div>
+          );
+        })}
       </div>
     </>
   );
