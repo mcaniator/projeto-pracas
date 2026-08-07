dfb14ef10ea7390d7b470049749fac758de94656
GuilhermeRPimenta <110068077+GuilhermeRPimenta@users.noreply.github.com>
2026-08-07 02:09:41 -0300

    refactor(route-handler-refactor): refactor fetchRecentActitity

--- src/lib/serverFunctions/queries/activity.ts
+++ src/lib/serverFunctions/queries/activity.ts
@@ -1,18 +1,34 @@
+import { APIResponseInfo } from "@/lib/types/backendCalls/APIResponse";
+
 import { fetchRecentlyCompletedAssessments } from "./assessment";
 import { fetchRecentlyCompletedTallys } from "./tally";
 
-export type FetchRecentActivityResponse = Awaited<
-  ReturnType<typeof fetchRecentActivity>
->;
+export type FetchRecentActivityResponse = NonNullable<
+  Awaited<ReturnType<typeof fetchRecentActivity>>
+>["data"];
 
 export const fetchRecentActivity = async () => {
-  const [assessments, tallys] = await Promise.all([
-    fetchRecentlyCompletedAssessments(),
-    fetchRecentlyCompletedTallys(),
-  ]);
+  try {
+    const [assessments, tallys] = await Promise.all([
+      fetchRecentlyCompletedAssessments(),
+      fetchRecentlyCompletedTallys(),
+    ]);
 
-  return {
-    assessments: assessments.data.assessments,
-    tallys: tallys.data.tallys,
-  };
+    return {
+      responseInfo: {
+        statusCode: 200,
+      } as APIResponseInfo,
+      data: {
+        assessments: assessments.data.assessments,
+        tallys: tallys.data.tallys,
+      },
+    };
+  } catch (e) {
+    return {
+      responseInfo: {
+        statusCode: 500,
+      } as APIResponseInfo,
+      data: null,
+    };
+  }
 };
